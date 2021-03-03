40c3644a3736ba33af1e9159d17fa1fe34d7a190
lordcoleton <Cole.Hoffman@comcast.net>
2021-03-03 13:07:03 -0500

    Initial Recomendation Research Commit

--- MakingRecomendations.py
+++ MakingRecomendations.py
@@ -6,4 +6,11 @@ print(critics['Lisa Rose']['Lady in the Water'])
 (critics['Toby']['Snakes on a Plane']==4.5)
 print(critics['Toby'])
 
-sqrt(pow(4.5-4,2)+pow(1-2,2))
+euclidDist = sqrt(pow(4.5-4,2)+pow(1-2,2))
+print("euclidian distance between 'Toby' and 'LaSalle' is:   ", euclidDist)
+
+
+
+
+
+
