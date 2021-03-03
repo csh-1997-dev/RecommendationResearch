b6f7a444a5370b148a8c4dd068913cfb76cd4fc6
lordcoleton <Cole.Hoffman@comcast.net>
2021-03-03 13:01:32 -0500

    Initial Recomendation Research Commit

---
+++ MakingRecomendations.py
@@ -0,0 +1,9 @@
+from Recomendations import critics
+from math import sqrt
+
+print(critics['Lisa Rose']['Lady in the Water'])
+
+(critics['Toby']['Snakes on a Plane']==4.5)
+print(critics['Toby'])
+
+sqrt(pow(4.5-4,2)+pow(1-2,2))
